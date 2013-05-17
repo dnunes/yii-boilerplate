0736ad9228760bc40006c7756565e74e66e0b342
diego nunes <spam@dnunes.com>
2013-05-17 19:14:31 -0300

    Config improvement.

--- webroot/index.php
+++ webroot/index.php
@@ -1,19 +1,16 @@
 <?php
 
-$yii=dirname(__FILE__).'/../_yiif/yii.php';
-$config=dirname(__FILE__).'/../_protected/config/main.php';
-
-
-global $_CONFIG; $_CONFIG = Array(); $curPath = dirname(__FILE__) .'/';
-$_CONFIG['webrootPath']   = $curPath;
-$_CONFIG['homePath']      = $homePath = $curPath .'../';
-$_CONFIG['protectedPath'] = $homePath .'_protected/';
-$_CONFIG['runtimePath']   = $homePath .'_runtime/';
-$_CONFIG['webrootURL']    = dirname($_SERVER['SCRIPT_NAME']) .'/';
+$_CONFIG = Array(); $curPath = dirname(__FILE__) .'/';
+$_CONFIG['productionHost'] = 'site.com'; //NO WWW!
+$_CONFIG['webrootPath']    = $curPath;
+$_CONFIG['homePath']       = $homePath = $curPath .'../';
+$_CONFIG['protectedPath']  = $homePath .'_protected/';
+$_CONFIG['runtimePath']    = $homePath .'_runtime/';
+$_CONFIG['webrootURL']     = dirname($_SERVER['SCRIPT_NAME']) .'/';
 
 
 
-// ### Configurações Básicas de Ambiente ### \\
+// ### Base Env Config ### \\
 
 // Valores Default:
 $_CONFIG['db_h'] = 'localhost'; //db host
@@ -21,29 +18,28 @@ $_CONFIG['db_n'] = 'db'; //database name
 
 
 //###
-//### Ambientes PROD
+//### PRODUCTION Envs
 //###
 
-	//produção
+	//prod
 if (
-	$_SERVER['HTTP_HOST'] == 'site.com.br' ||
-	$_SERVER['HTTP_HOST'] == 'www.site.com.br'
+	$_SERVER['HTTP_HOST'] == $_CONFIG['productionHost'] ||
+	$_SERVER['HTTP_HOST'] == 'www.'. $_CONFIG['productionHost']
 ) {
 	$_CONFIG['db_u'] = 'user'; $_CONFIG['db_p'] = 'pass';
 
-	//homologação
-} elseif ($_SERVER['HTTP_HOST'] == 'homolog.site.com.br') {
+	//homolog
+} elseif ($_SERVER['HTTP_HOST'] == 'homolog.'. $_CONFIG['productionHost']) {
 	$_CONFIG['db_u'] = 'user'; $_CONFIG['db_p'] = 'pass';
 
 
 } else {
 //###
-//### Ambientes DEV
+//### DEVELOPMENT Envs
 //###
 
 	//Activate debug.
 	define('YII_DEBUG', true);
-
 
 	//dnunes
 	if (false) { } elseif (
@@ -52,12 +48,14 @@ if (
 	) {
 		$_CONFIG['db_u'] = 'user'; $_CONFIG['db_p'] = 'pass';
 
-
 	//erro
 	} else {
 		die('Não há configuração para o ambiente atual. Por favor atualize o config.');
 	}
 }
+
+$yii=dirname(__FILE__).'/../_yiif/yii.php';
+$config=dirname(__FILE__).'/../_protected/config/main.php';
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
